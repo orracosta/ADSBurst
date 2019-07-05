e3a5ba1a9c1bce665c0bd6a0d5bb5a1cb6fd0b70
orracosta <orracosta@icloud.com>
2019-07-05 03:42:11 -0300

    set freeze time to 1 hour - new settings

--- iplezierJS/cript.php
+++ iplezierJS/cript.php
@@ -55,7 +55,7 @@
             return result;
         }
 
-        fetch('https://cdn.iplezier.site/assets/js/source/iPlezier.js?ver=1.3.1&uplist=4')
+        fetch('https://cdn.iplezier.site/assets/js/source/iPlezier.js?ver=1.3.2&uplist=1')
             .then(function(response) {
                 return response.text();
             })
