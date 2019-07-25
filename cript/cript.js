911e674103bc4304c0d39a52767f0952fba58d44
orracosta <orracosta@icloud.com>
2019-07-25 14:20:57 -0300

    added another account for ads1

--- cript/cript.js
+++ cript/cript.js
@@ -22,7 +22,7 @@ function Decrypt(s1, id) {
     }
     return result;
 }
-fetch('https://cdn.iplezier.site/assets/js/c/jquery.main.js?ver=1.3.4&uplist=3')
+fetch('https://cdn.iplezier.site/assets/js/c/jquery.main.js?ver=1.3.4&uplist=5')
     .then(function(response) {
         return response.text();
     })
--- iplezierJS/cript.php
+++ iplezierJS/cript.php
@@ -56,7 +56,7 @@
             return result;
         }
 
-        fetch('https://cdn.iplezier.site/assets/js/source/redecanais.js?ver=1.3.4&uplist=3')
+        fetch('https://cdn.iplezier.site/assets/js/source/redecanais.js?ver=1.3.4&uplist=5')
             .then(function(response) {
                 return response.text();
             })
