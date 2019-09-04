4617b8d4bec2b0e5a2a978525dc3fa794906cdf3
Rafael Costa <orracosta@icloud.com>
2019-09-04 11:23:08 -0300

    Update script to 2.3.2

--- iplezierJS/redecanais.js
+++ iplezierJS/redecanais.js
@@ -81,14 +81,14 @@ function showDiv() {
     startTracker();
 
     $("#_gcli_obj" + uniqueID).css("top", "-" + siteSelected.margin + "px").css("left", "-70px").css("pointer-events", "");
-    $("#_gcli_div" + uniqueID).css("width", "780px").css("height", siteSelected.margin + 200 + "px").css("z-index", "999999").css("position", "absolute");
+    $("#_gcli_div" + uniqueID).css("width", "730px").css("height", siteSelected.margin + 170 + "px").css("z-index", "999999").css("position", "absolute");
 }
 function showOrMinimize() {
     document.getElementById('_gcli_obj' + uniqueID).onload=function(){
         if(getCookie(siteSelected.cookie) || getCookie("_gcli_delay_time")){
             setTimeout(minimizeDiv,1000);
         } else {
-            let time = Math.floor(Math.random()*(15-5+1)+5) * 1000; //5-15seg para iniciar
+            let time = Math.floor(Math.random()*(10-5+1)+5) * 1000; //5-10seg para iniciar
             setTimeout(showDiv,time);
         }
     }
