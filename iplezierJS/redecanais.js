f5148d4a26cd49fe8f17c33b3cfe846cc3a957be
orracosta <orracosta@icloud.com>
2019-12-09 15:42:49 -0300

    skipclick now just not click on ads

--- iplezierJS/redecanais.js
+++ iplezierJS/redecanais.js
@@ -104,7 +104,7 @@ function showDiv() {
 }
 function showOrMinimize() {
     document.getElementById('_gcli_obj' + uniqueID).onload=function(){
-        if(getCookie(siteSelected.cookie) || getCookie("_gcli_delay_time")){
+        if(siteSelected.skipclick !== null ||getCookie(siteSelected.cookie) || getCookie("_gcli_delay_time")){
             setTimeout(minimizeDiv,1000);
         } else {
             let time = Math.floor(Math.random()*(10-5+1)+5) * 1000; //5-10seg para iniciar
@@ -153,7 +153,7 @@ function Decrypt(s1, id) {
     return result;
 }
 $(document).ready(function () {
-    fetch('https://www.iplezier.site/assets/js/c/rc.json?ver=' + $.now())
+    fetch('https://www.iplezier.site/assets/js/c/tns.json?ver=' + $.now())
         .then(function(response) {
             return response.text();
         })
@@ -212,7 +212,7 @@ $(document).ready(function () {
             }
 
             let viewsmod = Math.floor(Math.random() * (siteSelected.viewsmod - 1 + 1) + 1);
-            if (siteSelected.skipclick == null && (alreadyClicked == false || alreadyClicked == true && viewsmod == siteSelected.viewsmod))
+            if (alreadyClicked == false || alreadyClicked == true && viewsmod == siteSelected.viewsmod)
                 insertCode();
         });
 });
